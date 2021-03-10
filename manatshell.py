4636d9498b86917b2ee7510548d1120f48e5d13a
manatk <manatmadan@gmail.com>
2021-03-10 14:16:52 -0800

    testing if this works

--- manatshell.py
+++ manatshell.py
@@ -6,38 +6,76 @@ import glob
 
 class Job:
     def __init__(self, process, type):
-        self.test = 1
         self.process = process
         self.pid = process.pid
         self.status = process.poll
-        #self.children = []
         self.type = type
-#    def add_child(self,child_job):
-    #    self.children.append(child_job)
     def type():
         return "THIS IS A JOB OBJECT"
 
 #what do you do if parent process finishes before the child process?
 def clean_processes(running_processes):
     print(running_processes)
-    #processes_to_remove = []
     for job in running_processes:
-        for child_process in job.children:
-            if job_children.process.poll != None:
-                running_processes[job].remove(job_children.pid)
         if job.process.poll != None:
             print("FOUND FINISHED JOB")
             running_processes.remove(job)
 
+def launch_sbp(command_tokens, processes, input_redirection, output_redirection):
+    type = "foreground"
+    if "&" in command_tokens:
+        type = "background"
+    sbp = subprocess.Popen(split_commands_to_pipe[0], stdin=input_redirection, stdout=output_redirection)
+    child_job = Job(sbp,type)
+    processes.append(child_job)
+    return processes
+    if type == "foreground":
+        sbp.wait()
+
+def launch_piping(split_commands_to_pipe, processes):
+    type = "foreground"
+    if "&" in split_commands_to_pipe:
+        type = "background"
+
+    '''
+    s_in, s_out = (0, 0)
+    s_in = os.dup(0)
+    s_out = os.dup(1)
+
+    fdin = os.dup(s_in)
+    '''
+
+    #print(split_commands_to_pipe[0])
+    #sbp = subprocess.Popen(split_commands_to_pipe[0], stdin=subprocess.stdout, stdout=subprocess.PIPE)
+
+    for i in range(0, len(split_commands_to_pipe)):
+        print("EXECUTING", split_commands_to_pipe[i])
+        sbp = subprocess.Popen(split_commands_to_pipe[i], stdin=out, stdout=sbp.PIPE)
+        child_job = Job(sbp,type)
+        out = sbp.stdout
+
+    processes.append(child_job)
+    print("AFTER PIPING PROCESSes ", processes)
+    return processes
 
 def execute(command, processes):
     input_redirection = None
     output_redirection = None
     piping = False
 
-    #clean_processes(processes)
+    clean_processes(processes)
     command_tokens = split_line(command)
     if "|" in command_tokens:
+        piping = True
+        print("PIPING is true")
+        commands_to_pipe = (command.split("|"))
+        split_commands_to_pipe = []
+        for i in range(0,len(commands_to_pipe)):
+            split_commands_to_pipe.append(split_line(commands_to_pipe[i]))
+        command_tokens = split_commands_to_pipe
+        print(split_commands_to_pipe)
+
+        '''
         piping = True
         commands_to_pipe = []
         command_tokens_copy = [x for x in command_tokens]
@@ -50,6 +88,7 @@ def execute(command, processes):
         commands_to_pipe.append([command_tokens_copy[0]])
         print(commands_to_pipe)
         #subfile for each of the things you want do
+        '''
 
     if "<" in command_tokens:
         print("in the <")
@@ -84,21 +123,10 @@ def execute(command, processes):
             print("File was not successfully created")
             #continue
 
-        '''
-        index_value = command.index(">")
-        file = command_tokens[2]
-        command = command[0]
-        try:
-            output_redirection = open(file, 'r')
-        except:
-            print("File was not found")
-            #continue
-        '''
-
-    if command_tokens[0] == "pwd":
+    elif command_tokens[0] == "pwd":
         print(os.getcwd())
 
-    if command_tokens[0] == "cd":
+    elif command_tokens[0] == "cd":
         print("PATH IS", (command_tokens[len(command_tokens)-1]))
         try:
             os.chdir(command_tokens[1])
@@ -106,7 +134,7 @@ def execute(command, processes):
         except:
             print("please enter a valid path for where to go")
 
-    if command_tokens[0] == "bg":
+    elif command_tokens[0] == "bg":
         pid = command_tokens[1]
         print(pid)
         for job in processes:
@@ -115,13 +143,13 @@ def execute(command, processes):
                 print("stop signal sent")
                 job.process.send_signal(signal.SIGCONT)
 
-    if command_tokens[0] == "jobs":
+    elif command_tokens[0] == "jobs":
         #print(processes)
         for job in processes:
             if job.type == "background":
                 print(job.pid)
 
-    if command_tokens[0] == "fg":
+    elif command_tokens[0] == "fg":
         try:
             pid = command_tokens[1]
             print(pid)
@@ -133,32 +161,57 @@ def execute(command, processes):
             #how do you run in the foreground, make it p.wait()?
         except:
             "please enter valid arguments"
-
     else:
+        if piping == True:
+            processes = launch_piping(split_commands_to_pipe, processes)
+
+        else:
+            type = "foreground"
+            if "&" in command_tokens:
+                type = "background"
+            sbp = subprocess.Popen(command_tokens, stdin=input_redirection, stdout=output_redirection)
+            child_job = Job(sbp,type)
+            #current_pid = os.getpid()
+            #print(current_pid)
+            processes.append(child_job)
+            if type == "foreground":
+                sbp.wait()
+
+            #processes = launch_sbp(command_tokens,processes, input_redirection, output_redirection)
+
+        '''
         type = "foreground"
         if "&" in command_tokens:
             type = "background"
-        sbp = subprocess.Popen(command_tokens, stdin=input_redirection, stdout=output_redirection)
+
+        if piping == True:
+            print(split_commands_to_pipe[0])
+            sbp = subprocess.Popen(split_commands_to_pipe[i], stdin=sbp.stdout, stdout=sbp.PIPE)
+
+            for i in range(0, len(split_commands_to_pipe)):
+                old_process = process
+                sbp = subprocess.Popen(split_commands_to_pipe[i], stdin=sbp.stdout, stdout=sbp.PIPE)
+                child_job = Job(sbp,type)
+                out = process.stdout
+
+
+                processes.append(child_job)
+
+
+
+            #print("OD", sbp.output_redirection)
+            output_redirection = sbp.stdout
+            print("OD", output_redirection)
+
+        else:
+            sbp = subprocess.Popen(split_commands_to_pipe[0], stdin=input_redirection, stdout=output_redirection)
         child_job = Job(sbp,type)
         #current_pid = os.getpid()
         #print(current_pid)
         processes.append(child_job)
         if type == "foreground":
             sbp.wait()
-
         '''
-            if job.pid == current_pid:
-                processes[current_pid].add_child(child_job)
-                added = True
-        if added == False:
-            parent_job = Job(sbp,type)
-            processes.append(parent_job)
-        '''
-        #print(processes[0])
-        #print(processes[0].pid)
-        #print(processes[0].test)
-            #processes[current_pid] = child_job
-    #include a clean which looks through the processes and see if the job is complete, then remove. Kill the node and remove from the tree.
 
     return processes
 
